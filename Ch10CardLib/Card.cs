469ce3a742f5361466b9fb46b2168d5846b909a5
Adrian Girling <Adrian_girling@yahoo.co.uk>
2018-11-30 10:43:22 +0000

    Ch10

---
+++ Ch10CardLib/Card.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch10CardLib
+{
+    public class Card {
+        private readonly Rank rank;
+        private readonly Suit suit;
+
+        public Card(Suit newSuit, Rank newRank) {
+            // non-default public constructor - to force the use of a specific constructor
+            rank = newRank;
+            suit = newSuit;
+        }
+
+        private Card() {
+            // private default constructor
+        }
+
+        public override string ToString() {
+            return "The " + rank + " of " + suit + "s";
+        }
+    }
+}
